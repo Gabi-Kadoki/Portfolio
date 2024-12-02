b3087a931cf0991774ea25a2e03d05fef20d4cc9
Gabriela Kadoki <g.kadoki.dev@gmail.com>
2024-12-02 18:22:16 -0300

    2º versão
    Atualização do site, e algumas melhorias...

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -31,11 +31,34 @@ $(document).ready(function(){
     });
 });
 
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+
 window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('custom-navbar');
+    const navbar = document.querySelector('.custom-navbar');
     if (window.scrollY > 0) {
-        custom-navbarr.classList.add('scrolled'); // Adiciona a classe quando rola
+      navbar.classList.add('navbar-scrolled');   
+  
     } else {
-        custom-navbar.classList.remove('scrolled'); // Remove a classe quando volta ao topo
+      navbar.classList.remove('navbar-scrolled');
     }
-  }); 
+});
+
+// Aguardar o carregamento do DOM
+document.addEventListener('DOMContentLoaded', function () {
+const navbar = document.querySelector('.custom-navbar');
+// Função para adicionar/remover a classe 'scrolled' com base no scroll
+function handleScroll() {
+    if (window.scrollY > 20) {
+        navbar.classList.add('scrolled');  // Adiciona a classe 'scrolled' quando rolar
+    } else {
+        navbar.classList.remove('scrolled');  // Remove a classe 'scrolled' quando voltar ao topo
+    }
+}   
+// Adiciona o evento de scroll
+window.addEventListener('scroll', handleScroll);
+});
